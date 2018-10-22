bc2350f42ad85a68c922ae5520881f7f00be083f
Yuanmiao Gui <gui_yuan_miao@163.com>
2018-10-22 09:00:28 +0800

    add bat script for mingw32 and mingw64

---
+++ .mingw64.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+set THIS_DIR=%~dp0
+cd "%THIS_DIR%"
+
+if "x%MSYS2_HOME%" == "x" set MSYS2_HOME=
+set MSYS2_SHELL=%MSYS2_HOME%\msys2_shell.cmd
+
+if NOT EXIST %MSYS2_SHELL% goto notfound
+"%MSYS2_SHELL%" -mingw64 -use-full-path -here
+REM "%MSYS2_SHELL%" -mingw64 -use-full-path -where "%THIS_DIR%"
+goto EOF
+
+:notfound
+echo Cannot find %MSYS2_SHELL% -- aborting.
+pause
+
+:EOF
